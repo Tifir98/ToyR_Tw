84fc5a92fa91254a25e2fda1a7a4d94346d7bc59
andronicalex <andronic.alex@yahoo.com>
2019-06-13 02:30:26 +0300

    Update removeUser.php

--- removeUser.php
+++ removeUser.php
@@ -95,7 +95,7 @@
    <div class="container" style="width:100%;">  
    <h3 align="center">Users:</h3><br /> 
         <div class="table-responsive">  
-        <table class="table table-bordered">  
+        <table class="table table-bordered" border="1">  
         <tr>  
              <th width="25%">Email</th>  
              <th width="25%">Nume</th>  
